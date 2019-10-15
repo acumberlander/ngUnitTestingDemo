56674f53878b4d45d2d4cef34092f2270a8e34f1
Austin Cumberlander <austincumberlander@gmail.com>
2019-10-15 13:36:45 -0500

    used mocks, isolated unit tests, and shallow integrationtests

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,30 @@
+import { MessageService } from './message.service';
+
+describe('MessageService', () => {
+    let service: MessageService;
+
+    beforeEach(() => {
+        service = new MessageService();
+    });
+
+    it('should have no message to start', () => {
+        expect(service.messages.length).toBe(0);
+    });
+
+    it('should add a message when add is called', () => {
+        service = new MessageService();
+
+        service.add('message1');
+
+        expect(service.messages.length).toBe(1);
+    });
+
+    it('should remove all message when clear is called', () => {
+        service = new MessageService();
+        service.add('message1');
+
+        service.clear();
+
+        expect(service.messages.length).toBe(0);
+    });
+});
